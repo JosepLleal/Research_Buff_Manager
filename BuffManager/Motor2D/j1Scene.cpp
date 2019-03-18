84db96e900996f2ee621565ad418928dac5f014c
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2019-03-18 23:34:54 +0100

    Added base code with stl

---
+++ BuffManager/Motor2D/j1Scene.cpp
@@ -0,0 +1,103 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Input.h"
+#include "j1Textures.h"
+#include "j1Audio.h"
+#include "j1Render.h"
+#include "j1Window.h"
+#include "j1Map.h"
+#include "j1Scene.h"
+
+j1Scene::j1Scene() : j1Module()
+{
+	name.assign("scene");
+}
+
+// Destructor
+j1Scene::~j1Scene()
+{}
+
+// Called before render is available
+bool j1Scene::Awake(pugi::xml_node& config)
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	for (pugi::xml_node stage = config.child("map_name"); stage; stage = stage.next_sibling("map_name"))
+	{
+		std::string* StageName = new std::string(stage.attribute("path").as_string());
+		StageList.push_back(StageName);
+	}
+
+	return ret;
+}
+
+// Called before the first frame
+bool j1Scene::Start()
+{
+	// --- Loading map ---
+
+	App->map->Load(StageList.front()->data());
+
+	debug_tex = App->tex->Load("maps/path2.png");
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::Update(float dt)
+{
+	if(App->input->GetKey(SDL_SCANCODE_L) == KEY_DOWN)
+		App->LoadGame("save_game.xml");
+
+	if(App->input->GetKey(SDL_SCANCODE_S) == KEY_DOWN)
+		App->SaveGame("save_game.xml");
+
+	
+	/*if(App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		App->render->camera.y += ceil(150.0*dt);
+
+	if(App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		App->render->camera.y -= ceil(150.0*dt);
+
+	if(App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		App->render->camera.x += ceil(150.0*dt);
+
+	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		App->render->camera.x -= ceil(150.0*dt);*/
+
+	App->map->Draw();
+
+	
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PostUpdate()
+{
+	bool ret = true;
+
+	if(App->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+// Called before quitting
+bool j1Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	std::vector<std::string*>().swap(StageList);
+
+	return true;
+}
+
